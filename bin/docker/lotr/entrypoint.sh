084b1db01268bca4f72e3d77739edab47f114322
Pedro Rico Pinazo <ricopinazo@gmail.com>
2022-06-21 18:19:51 +0200

    Add dependencies using pulsar healthcheck

--- bin/docker/lotr/entrypoint.sh
+++ bin/docker/lotr/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 component=$1
 
-set -eux
-
-declare HOST="$RAPHTORY_PULSAR_ADMIN_ADDRESS/admin/v2/worker/cluster"
-declare STATUS=200
-declare TIMEOUT=1000
-
-HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
-    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
-        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
-        echo "received status: $STATUS_RECEIVED" && \
-        sleep 1;\
-    done;
-    echo success with status: $STATUS_RECEIVED'
+#set -eux
+#
+#declare HOST="$RAPHTORY_PULSAR_ADMIN_ADDRESS/admin/v2/worker/cluster"
+#declare STATUS=200
+#declare TIMEOUT=1000
+#
+#HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
+#    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
+#        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
+#        echo "received status: $STATUS_RECEIVED" && \
+#        sleep 1;\
+#    done;
+#    echo success with status: $STATUS_RECEIVED'
 
 if [ $1 = "client" ]
 then
