52df20ea349c30dc4d4a77873188b39c7cb55851
rachelchan <rachelchan1497@gmail.com>
2022-07-21 14:03:54 +0100

    change to edgelist

--- connectors/aws/src/main/scala/com/raphtory/aws/AwsSpoutTest.scala
+++ connectors/aws/src/main/scala/com/raphtory/aws/AwsSpoutTest.scala
@@ -31,7 +31,8 @@ def main(args: Array[String]) {
   val output = FileSink("/tmp/test")
   val graph = Raphtory.load[String](source, builder)
     graph
-      .execute(EdgeList() -> VertexFilter(vertex => vertex.outDegree > 100))
+//      .execute(VertexFilter(vertex => vertex.outDegree > 100))
+      .execute(EdgeList())
       .writeTo(output)
       .waitForJob()
 
