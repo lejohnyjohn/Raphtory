77e3337b7bbde6212560fe4beb26f411cacb23ca
rachelchan <rachelchan1497@gmail.com>
2022-07-21 13:23:59 +0100

    generic

--- connectors/aws/src/main/scala/com/raphtory/aws/FilteredOutDegree.scala
+++ connectors/aws/src/main/scala/com/raphtory/aws/FilteredOutDegree.scala
@@ -1,10 +1,11 @@
 package com.raphtory.aws
 
 import com.raphtory.algorithms.generic.NodeList
+import com.raphtory.api.analysis.algorithm.Generic
 import com.raphtory.api.analysis.graphview.GraphPerspective
 import com.raphtory.api.analysis.table.{Row, Table}
 
-class FilteredOutDegree extends NodeList(Seq("filteredOutDegree")) {
+class FilteredOutDegree extends Generic {
 
   override def apply(graph: GraphPerspective): graph.Graph = OutDegree(graph)
 
